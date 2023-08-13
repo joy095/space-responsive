5c15f96f876112067206ec7d02f0deccd0f5cd87
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-08-13 17:42:46 +0530

    update

---
+++ script.js
@@ -0,0 +1,19 @@
+var header = document.getElementById("header-links");
+var btns = header.getElementsByClassName("link");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+var destination = document.getElementById("destination-link-wrap");
+var btns = destination.getElementsByClassName("destination-link");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
