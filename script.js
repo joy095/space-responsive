a8999dba2aa9c3c8a68be86c2b3e428ab7bd9cca
Joy Karmakar <96533682+joy095@users.noreply.github.com>
2023-08-26 13:32:57 +0530

    technologu subpage and mobile tab navigation done

--- script.js
+++ script.js
@@ -17,3 +17,24 @@ for (var i = 0; i < btns.length; i++) {
         this.className += " active";
     });
 }
+
+// header-links-mobile link
+var links = document.getElementById("header-links-mobile");
+var btns = links.getElementsByClassName("link-stage");
+for (var i = 0; i < btns.length; i++) {
+    btns[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
+
+// header-links-mobile handburger
+function toggle() {
+    var x = document.getElementById("header-links-mobile");
+    if (x.style.display === "flex") {
+        x.style.display = "none";
+    } else {
+        x.style.display = "flex";
+    }
+}
